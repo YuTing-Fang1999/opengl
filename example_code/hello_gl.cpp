8518d8a928d6c86133caa59e043b66597a97bbfb
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-10-23 12:10:58 +0800

    Create hello_gl.cpp

---
+++ example_code/hello_gl.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+using namespace std;
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+
+	glutCreateWindow("hello, gl");
+	// Bind the two functions (above) to respond when necessary
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(render);
+
+	// Very important!  This initializes the entry points in the OpenGL driver so we can 
+	// call all the functions in the API.
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+
+
+	glutMainLoop();
+	return 0;
+}
